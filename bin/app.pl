ab2e45e73586ad462f15d82a39679cf86a0f28b0
Ryan Voots <simcop2387@simcop2387.info>
2020-07-29 23:39:02 -0400

    Now builds inside minion

--- bin/app.pl
+++ bin/app.pl
@@ -5,10 +5,51 @@ use warnings;
 
 use FindBin;
 use lib $FindBin::Bin.'/../lib';
+use Path::Tiny;
 
 use MyMinion;
+use BuildPerlTask;
+use Function::Parameters qw/:std/; # TODO remove this
 
 # TODO enqueue jobs
+
+my %args; # TODO parse from cli
+
+# TODO move these two functions to a library
+fun get_baseid($time, $branch, $randid) {
+    return sprintf "%s-%s-%s", $branch, $time, $randid;
+}
+
+fun get_perl_id($time, $branch, $randid, $opts) {
+  my $baseid = get_baseid($time, $branch, $randid);
+
+  for my $k (qw(threads quadmath)) {
+    next if !defined $opts->{$k} || !$opts->{$k};
+    $baseid .= "-$k";
+  }
+
+  return $baseid;
+}
+
+my $time = $args{time} // Time::Moment->now()->strftime("%Y-%m-%d");
+my $randid = $args{randid} // join('', map {chr(65+rand()*26)} 1..5);
+my $branch = $args{branch} // "blead";
+my $basepath = path($args{basepath} // '/home/perlbot/perl5/custom/');
+my $srcpath = path($args{srcpath} // '/home/perlbot/build/perl5');
+
+my $baseid = get_baseid($time, $branch, $randid);
+my $perlid = get_perl_id($time, $branch, $randid, {threads => 0});
+
+my $prev_perl;
+
+for my $opts ({threads => 0}, {threads => 1}) {
+  my $basenotes = {$perlid => 1, srcpath => $srcpath, basepath=>$basepath, options => $opts};
+
+  my $build_id = BuildPerlTask::build_perl($srcpath, $basepath, $perlid, $branch, $opts, $basenotes, $prev_perl);
+  $prev_perl = $build_id;
+
+  # TODO schedule cpanm installs
+}
 
 $minion->perform_jobs();
 
---
+++ lib/BuildPerlTask.pm
@@ -0,0 +1,141 @@
+package BuildPerlTask;
+
+use strict;
+use warnings;
+
+use MyMinion;
+use Path::Tiny;
+use Runner;
+use Git::Wrapper;
+use Perl::Build;
+use Time::Moment;
+use Syntax::Keyword::Try;
+use Data::Dumper;
+
+$minion->add_task(clean_git => sub {
+  my ($job, $srcpath, $perlid) = @_;
+  # Wait until we're done with the git repo
+  sleep 1 until $minion->lock('git_repo_lock_'.$srcpath, 7200, {limit => 1});
+  sleep 1 until $minion->lock('build_'.$perlid);
+    
+  my $git = Git::Wrapper->new({dir => $srcpath});
+
+  # TODO log this
+  # $logger->debug("gitclean", $perlid, {line => "Cleaning git..."});
+
+  try {
+    $git->clean({f => 1, d => 1});
+  } catch {
+    #$logger->debug("gitclean", $perlid, {line => $@->output, time => time(), channel => "stdout"});
+    #$logger->error("gitclean", $perlid, {line => $@->error, time => time(), channel => "stderr"});
+    #$logger->debug("gitclean", $perlid, {line => $@->status, time => time(), channel => "cmdstatus"});
+    $minion->unlock("git_repo_lock_".$srcpath);
+    $minion->unlock("build_".$perlid);
+    die "Failed to git ".$@->error;
+  # Don't unlock the repo yet!
+  }
+
+  # TODO check the output of this
+  Runner::run_code(
+    code => sub {
+      chdir $srcpath;
+      system("make clean");
+    }, 
+    timeout => 240, 
+    cgroup => "make-clean-$perlid",
+    stdin => "", 
+    #logger => sub {$logger->debug("makeclean", $perlid, @_)}
+  );
+  
+  $job->finish("Cleaned git for $perlid in $srcpath");
+});
+
+$minion->add_task(checkout_git => sub {
+  my ($job, $srcpath, $perlid, $refid) = @_;
+
+  if ($minion->lock('git_repo_lock_'.$srcpath, 0) || $minion->lock('git_repo_lock_'.$srcpath, 0)) {
+    my $repolock = $minion->lock('git_repo_lock_'.$srcpath, 0);
+    my $buildlock = $minion->lock('build_'.$perlid, 0);
+    
+    if ($repolock) {
+      $minion->unlock('build_'.$perlid);
+    }
+
+    die "Locks were invalidated before checkout $srcpath $perlid";
+  }
+
+  my $git = Git::Wrapper->new({dir => $srcpath});
+
+  try {
+    $git->checkout($refid);
+  } catch {
+    #$logger->debug("gitcheckout", $perlid, {line => $git->output, time => time(), channel => "stdout"});
+    #$logger->error("gitcheckout", $perlid, {line => $git->error, time => time(), channel => "stderr"});
+    #$logger->debug("gitcheckout", $perlid, {line => $git->status, time => time(), channel => "cmdstatus"});
+    $minion->unlock("git_repo_lock_".$srcpath);
+    $minion->unlock("build_".$perlid);
+
+    die "Failed to git checkout ".$@->error;
+  }
+
+  $job->finish("Successfully checked out $refid");
+});
+
+$minion->add_task(build_perl => sub {
+  my ($job, $srcpath, $perlid, $basepath, $opts) = @_;
+
+  if ($minion->lock('git_repo_lock_'.$srcpath, 0) || $minion->lock('git_repo_lock_'.$srcpath, 0)) {
+    my $repolock = $minion->lock('git_repo_lock_'.$srcpath, 0);
+    my $buildlock = $minion->lock('build_'.$perlid, 0);
+    
+    if ($repolock) {
+      $minion->unlock('build_'.$perlid);
+    }
+
+    die "Locks were invalidated before build $srcpath $perlid";
+  }
+  
+  try {
+    my $dst = path($basepath)->child($perlid);
+
+    my $ret_data = Runner::run_code(code => sub {
+      Perl::Build->install(
+        src_path => $srcpath,
+        dst_path => $dst,
+        configure_options => [
+          '-de',
+          '-Dusedevel',
+          '-Accflags="-fpie -fPIC -mtune=native -fstack-protector-all -pie -D_FORTIFY_SOURCE=2 -ggdb  -DPERL_EMERGENCY_SBRK"',
+          '-Aldflags="-Wl,-z,now -Wl,-zrelro -Wl,-z,noexecstack"',
+          '-Duseshrplib',
+          '-Dusemymalloc=y',
+          '-Uversiononly',
+          ($opts->{threads} ? '-Dusethreads' : ()),
+        ],
+        test => 1,
+        jobs => 4
+      );
+
+      $dst->child('.tested')->touch();
+    }, logger => sub {print "$perlid: ".$_[0]->{line}; }, timeout => 60*60, cgroup => "canary-$perlid", stdin => "");
+  } catch {
+    die $@; # rethrow everything
+  } finally {
+    # always unlock the repo
+    $minion->unlock('git_repo_lock_'.$srcpath);
+    $minion->unlock('build_'.$perlid);
+  }
+
+});
+
+sub build_perl {
+  my ($srcpath, $basepath, $perlid, $branch, $opts, $basenotes, $real_parent) = @_;
+
+  my $cleangit_id = $minion->enqueue(clean_git => [$srcpath, $perlid] => {notes => $basenotes, parents => [$real_parent // ()]});
+  my $checkoutgit_id = $minion->enqueue(checkout_git => [$srcpath, $perlid, $branch] => {notes => $basenotes, parents => [$cleangit_id]});
+  my $build_id = $minion->enqueue(build_perl => [$srcpath, $perlid, $basepath, $opts] => {notes => $basenotes, parents => [$checkoutgit_id]});
+
+  return $build_id;
+}
+
+1;
